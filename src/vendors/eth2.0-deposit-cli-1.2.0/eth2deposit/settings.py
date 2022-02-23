43a307313935478b430fb3571ac73570abbc66c4
Alex Peterson <alexpeterson91@gmail.com>
2022-02-22 20:26:09 -0600

    Add Gnosis Chain to backend settings.py
    added gnosis chain backend settings.py

--- src/vendors/eth2.0-deposit-cli-1.2.0/eth2deposit/settings.py
+++ src/vendors/eth2.0-deposit-cli-1.2.0/eth2deposit/settings.py
@@ -1,5 +1,5 @@
 from typing import Dict, NamedTuple
-
+from os import environ
 
 DEPOSIT_CLI_VERSION = '1.2.0'
 
@@ -12,7 +12,9 @@ class BaseChainSetting(NamedTuple):
 MAINNET = 'mainnet'
 PYRMONT = 'pyrmont'
 PRATER = 'prater'
-
+GNOSIS_TESTNET = 'gnosis-testnet'
+GNOSIS = 'gnosis'
+TEST = 'test'
 
 # Eth2 Mainnet setting
 MainnetSetting = BaseChainSetting(ETH2_NETWORK_NAME=MAINNET, GENESIS_FORK_VERSION=bytes.fromhex('00000000'))
@@ -20,12 +22,20 @@ MainnetSetting = BaseChainSetting(ETH2_NETWORK_NAME=MAINNET, GENESIS_FORK_VERSIO
 PyrmontSetting = BaseChainSetting(ETH2_NETWORK_NAME=PYRMONT, GENESIS_FORK_VERSION=bytes.fromhex('00002009'))
 # Eth2 testnet (spec v1.0.1)
 PraterSetting = BaseChainSetting(ETH2_NETWORK_NAME=PRATER, GENESIS_FORK_VERSION=bytes.fromhex('00001020'))
+# Gnosis Beacon Chain testnet setting
+GnosisTestnetSetting = BaseChainSetting(NETWORK_NAME=GNOSIS_TESTNET, GENESIS_FORK_VERSION=bytes.fromhex('00006464'))
+# Gnosis Beacon Chain setting
+GnosisSetting = BaseChainSetting(NETWORK_NAME=GNOSIS, GENESIS_FORK_VERSION=bytes.fromhex('00000064'))
 
+TestSetting = BaseChainSetting(NETWORK_NAME=TEST, GENESIS_FORK_VERSION=bytes.fromhex(environ.get('GENESIS_FORK_VERSION', '12345678')))
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
     MAINNET: MainnetSetting,
     PYRMONT: PyrmontSetting,
     PRATER: PraterSetting,
+    GNOSIS_TESTNET: GnosisTestnetSetting,
+    GNOSIS: GnosisSetting,
+    TEST: TestSetting,
 }
 
 
